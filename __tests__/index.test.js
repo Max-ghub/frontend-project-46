953a369b246adce5ee8b4804a381385b0620cd36
Max <max@mail.ru>
2022-09-13 09:01:06 +0300

    step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,12 +9,21 @@ const __dirname = path.dirname(__filename);
 
 const getPath = (file) => path.join(__dirname, '../__fixtures__', file);
 
-test('Difference', () => {
-  const testFile1Path = getPath('file1.json');
-  const testFile2Path = getPath('file2.json');
+const resultFile = readFileSync(getPath('result.txt'), 'utf-8').replaceAll('\r', '');
+test('JSON difference', () => {
+  const JSON1Path = getPath('file1.json');
+  const JSON2Path = getPath('file2.json');
+  const JSONgenDiff = genDiff(JSON1Path, JSON2Path);
+  expect(JSONgenDiff).toEqual(resultFile);
+});
+test('YAML/YML difference', () => {
+  const YML1Path = getPath('file1.yml');
+  const YML2Path = getPath('file2.yml');
+  const YMLgenDiff = genDiff(YML1Path, YML2Path);
+  expect(YMLgenDiff).toEqual(resultFile);
 
-  const text = genDiff(testFile1Path, testFile2Path);
-  const result = readFileSync(getPath('result.txt'), 'utf-8').replaceAll('\r', '');
-
-  expect(text).toEqual(result);
+  const YAML1Path = getPath('file1.yaml');
+  const YAML2Path = getPath('file2.yaml');
+  const YAMLgenDiff = genDiff(YAML1Path, YAML2Path);
+  expect(YAMLgenDiff).toEqual(resultFile);
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import path from 'node:path';
 import { readFileSync } from 'node:fs';
 import _ from 'lodash';
+import parseData from './parsers.js';
 
+// const getAbslPath = (fileName) => path.resolve(process.cwd(), `__fixtures__/${fileName}`);
 const getAbslPath = (fileName) => path.resolve(process.cwd(), fileName);
 
-const parseFile = (file) => JSON.parse(file);
-
-const getDataDifferences = (firstFileData, secondFileData) => {
-  const firstDataParsed = parseFile(firstFileData);
+const getDataDifferences = (firstFile, secondFile) => {
+  const firstDataParsed = parseData(firstFile);
   const firstDataKeys = _.keys(firstDataParsed);
 
-  const secondDataParsed = parseFile(secondFileData);
+  const secondDataParsed = parseData(secondFile);
   const secondDataKeys = _.keys(secondDataParsed);
 
   const unionDataKeys = _.union(firstDataKeys, secondDataKeys);
@@ -70,16 +70,18 @@ const createTextDifferencesOutput = (differences) => {
 };
 
 const genDiff = (firstFileName, secondFileName) => {
-  const firstFilePath = getAbslPath(firstFileName);
-  const firstFileData = readFileSync(firstFilePath, 'utf-8');
+  const firstFile = { path: getAbslPath(firstFileName) };
+  firstFile.format = path.extname(firstFile.path).slice(1);
+  firstFile.data = readFileSync(firstFile.path, 'utf-8');
 
-  const secondFilePath = getAbslPath(secondFileName);
-  const secondFileData = readFileSync(secondFilePath, 'utf-8');
+  const secondFile = { path: getAbslPath(secondFileName) };
+  secondFile.format = path.extname(secondFile.path).slice(1);
+  secondFile.data = readFileSync(secondFile.path, 'utf-8');
 
-  const differences = getDataDifferences(firstFileData, secondFileData);
-  const differencesOutput = createTextDifferencesOutput(differences);
+  const filesDifferences = getDataDifferences(firstFile, secondFile);
+  const outputDifferences = createTextDifferencesOutput(filesDifferences);
 
-  return differencesOutput;
+  return outputDifferences;
 };
-
+// console.log(genDiff('file1.json', 'file2.json'));
 export default genDiff;
